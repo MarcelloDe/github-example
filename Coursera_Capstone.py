4edfd28392c53bf854e936f03c636f50e53b7664
MarcelloDe <59294768+MarcelloDe@users.noreply.github.com>
2020-03-21 22:34:48 -0400

    Add files via upload

---
+++ Coursera_Capstone.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+#<p> This notebook is for the Data Science capstone <p>
+print(" Hello Capstone Project Course!")
+
+
+# In[11]:
+
+
+import pandas as pd
+
+
+# In[12]:
+
+
+import numpy as np
+
+
+# In[ ]:
+
+
+
+
